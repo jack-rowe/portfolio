735c198de7f865fe1f95253353890280223298ad
Jack Rowe <rowejackj@gmail.com>
2025-01-20 19:20:46 -0500

    netlify setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div className="bg-secondary h-full w-full"></div>;
+  return (
+    <div className="bg-secondary h-screen w-full ">
+      {/* nothing here message */}
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="text-3xl font-bold">There is nothing here</h1>
+        <p className="text-lg">for now...</p>
+      </div>
+    </div>
+  );
 }
