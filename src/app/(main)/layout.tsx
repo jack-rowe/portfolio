9e291f61d9c801bb9b6acbbcf41df4aa201f8f67
Jack Rowe <rowejackj@gmail.com>
2025-12-28 00:07:59 -0500

    start of v2

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,47 +1,16 @@
 "use client";
 
-import { motion } from "motion/react";
-import dynamic from "next/dynamic";
-import { ReactNode, useEffect, useState } from "react";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-
-const SplashScreen = dynamic(() => import("@/components/SplashScreen"), {
-  ssr: false,
-});
+import { ReactNode } from "react";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
-  const [isLoading, setIsLoading] = useState(true);
-
-  const isDev = process.env.NEXT_PUBLIC_ENV === "development";
-
-  useEffect(() => {
-    const timer = setTimeout(
-      () => {
-        setIsLoading(false);
-      },
-      isDev ? 0 : 2000
-    ); // Adjust timing as needed
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <>
       <Analytics />
       <SpeedInsights />
-      <SplashScreen isLoading={isLoading} />
-      {!isLoading && (
-        <motion.main
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.5 }}
-          className="bg-gray-950"
-        >
-          {children}
-        </motion.main>
-      )}
+
+      <main>{children}</main>
     </>
   );
 }
